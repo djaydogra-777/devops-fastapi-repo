4a5bd63a1f70eaabc7bd4b6eb86d97c463fa19d1
djaydogra-777 <dhananjay.dogra@siriuscom.com>
2026-04-20 22:54:12 +0530

    logger new update

--- app/logger.py
+++ app/logger.py
@@ -1,33 +1,33 @@
 import logging
 import os
-from logging import LoggerAdapter
 
-# Environment
 ENV = os.getenv("ENV", "dev")
 
-# Formatter (simple + works well with Loki)
 formatter = logging.Formatter(
     "%(asctime)s - %(levelname)s - %(message)s"
 )
+
+class ServiceLogger(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+        if "extra" not in kwargs:
+            kwargs["extra"] = {}
+        kwargs["extra"]["service"] = self.extra["service"]
+        return msg, kwargs
 
 
 def create_logger(service_name: str):
     base_logger = logging.getLogger(service_name)
     base_logger.setLevel(logging.INFO)
 
-    # Avoid duplicate handlers
     if base_logger.handlers:
         return base_logger
 
-    # Console handler (always enabled)
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
     base_logger.addHandler(console_handler)
 
-    # Attach service context
-    logger = LoggerAdapter(base_logger, {"service": service_name})
+    logger = ServiceLogger(base_logger, {"service": service_name})
 
-    # Loki handler only in prod
     if ENV == "prod":
         try:
             from logging_loki import LokiHandler
@@ -35,8 +35,7 @@ def create_logger(service_name: str):
             loki_handler = LokiHandler(
                 url="http://loki-gateway.loki.svc.cluster.local/loki/api/v1/push",
                 tags={
-                    "service": service_name,
-                    "env": ENV,
+                    "env": ENV
                 },
                 version="1",
             )
@@ -50,5 +49,4 @@ def create_logger(service_name: str):
     return logger
 
 
-# Default logger used across app
 logger = create_logger("simple-fastapi-app")
