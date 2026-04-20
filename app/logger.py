51be67682f1667ab727a33f3a65ad91c23de33de
djaydogra-777 <dhananjay.dogra@siriuscom.com>
2026-04-20 22:43:11 +0530

    logger updated

--- app/logger.py
+++ app/logger.py
@@ -1,45 +1,35 @@
-import logging
-import os
-import json
+from logging import LoggerAdapter
 
-logger = logging.getLogger("fastapi-app")
-logger.setLevel(logging.INFO)
+def create_logger(service_name: str):
+    base_logger = logging.getLogger(service_name)
+    base_logger.setLevel(logging.INFO)
 
-class JsonFormatter(logging.Formatter):
-    def format(self, record):
-        return json.dumps({
-            "timestamp": self.formatTime(record),
-            "level": record.levelname,
-            "message": record.getMessage(),
-            "service": "simple-fastapi-app"
-        })
+    if base_logger.handlers:
+        return base_logger
 
-formatter = JsonFormatter()
-
-# Avoid duplicate handlers
-if not logger.handlers:
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+    base_logger.addHandler(console_handler)
 
-ENV = os.getenv("ENV", "dev")
+    logger = LoggerAdapter(base_logger, {"service": service_name})
 
-if ENV == "prod":
-    try:
-        from logging_loki import LokiHandler
+    if ENV == "prod":
+        try:
+            from logging_loki import LokiHandler
 
-        loki_handler = LokiHandler(
-            url="http://loki-gateway.loki.svc.cluster.local/loki/api/v1/push",
-            tags={
-                "service": "simple-fastapi-app",
-                "env": "prod"
-            },
-            version="1",
-            headers={"X-Scope-OrgID": "tenant1"},
-        )
+            loki_handler = LokiHandler(
+                url="http://loki-gateway.loki.svc.cluster.local/loki/api/v1/push",
+                tags={
+                    "service": service_name,
+                    "env": ENV
+                },
+                version="1",
+            )
 
-        loki_handler.setFormatter(formatter)
-        logger.addHandler(loki_handler)
+            loki_handler.setFormatter(formatter)
+            base_logger.addHandler(loki_handler)
 
-    except Exception as e:
-        logger.error(f"Failed to initialize Loki handler: {e}")
+        except Exception as e:
+            base_logger.error(f"Loki init failed: {e}")
+
+    return logger
